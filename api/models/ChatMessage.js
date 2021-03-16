1154ff9cf315c8864f8078ac19e1a48979860af7
GrigorisPs <73849201+GrigorisPs@users.noreply.github.com>
2021-03-16 20:59:39 +0200

    added ChatMessage model and changed migration to safe

--- api/models/ChatMessage.js
+++ api/models/ChatMessage.js
@@ -1,42 +1,26 @@
 /**
- * User.js
+ * ChatMessage.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
 module.exports = {
-  tableName: 'users',
-  attributes: {
-    username: {
-      type: 'string',
-      required: true
-    },
 
-    online: {
-      type: 'boolean',
-      defaultsTo: true
-    },
+  attributes: {
 
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-    firstName: { type: 'string' },
-    lastName: { type: 'string' },
-    email: { type: 'string' },
-    password: { type: 'string' },
-    purchases: {
-      collection: 'Purchase',
-      via: 'userId',
+
+    user: {
+      model: 'User'
     },
-    chats: {
-      collection: 'ChatMessage',
-      via: 'user'
+
+    text: {
+      type: 'string',
+      required: true
     }
-
-
-
-
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
